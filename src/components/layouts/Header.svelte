fb446b4c6891703784c2694b42e969623ea0804d
anton.miroshnichenko <anton.miroshnichenko@omnigon.com>
2019-06-17 12:02:26 +0300

    Header

--- src/components/layouts/Header.svelte
+++ src/components/layouts/Header.svelte
@@ -1,13 +1,15 @@
 <script>
     import {onMount} from 'svelte';
     import navigation from '../../stores/navigation';
-    import {Link} from 'svelte-routing';
+    import HeaderLink  from './HeaderLink.svelte';
 
     const {loading, error} = navigation;
 
     onMount(async function() {
        await navigation.fetch();
     });
+
+    $: root = {title: '', items: $navigation, isRoot: true};
 </script>
 
 <style>
@@ -15,8 +17,6 @@
        --navigatonBackground: #ccc;
        --navigationHeight: 40px;
        --loadingColor: #909090;
-       --linkColor: #5399cc;
-       --linkColorHovered: #447aa3;
        --errorColor: #e93d5f;
     }
 
@@ -38,25 +38,6 @@
         height: var(--navigationHeight);
         background: var(--navigatonBackground);
     }
-
-    ul {
-        margin: 0;
-        padding: 10px;
-        list-style: none;
-    }
-
-    ul li {
-        display: inline-block;
-        margin-left: 10px;
-    }
-
-    ul li a {
-        color: var(--linkColor)
-    }
-
-    ul li a:hover {
-        color: var(--linkColorHovered);
-    }
 </style>
 
 <nav>
@@ -65,13 +46,7 @@
     {:else if $error}
         <div class="error">{$error}</div>
     {:else}
-        <ul>
-            {#each $navigation as item (item.id)}
-                <li>
-                    <Link to={item.href} >{item.title}</Link>
-                </li>
-            {/each}
-        </ul>
+        <HeaderLink item={root} />
     {/if}
 
 </nav>
---
+++ src/components/layouts/HeaderLink.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let item;
+    import {Link} from 'svelte-routing';
+</script>
+
+<style>
+    .is-root {
+        width: 100%;
+        display: block;
+    }
+
+    .is-root ul {
+        width: 100%;
+        padding: 10px 0;
+        margin: 0;
+        list-style: none;
+    }
+
+    .is-root li {
+        display: inline-block;
+        margin-left: 10px;
+    }
+</style>
+
+{#if item}
+    {#if item.items }
+        <span class:is-root={item.isRoot}>
+            {item.title}
+            <ul>
+                {#each item.items as item (item.id)}
+                    <svelte:self item={item}></svelte:self>
+                {/each}
+            </ul>
+        </span>
+    {:else}
+        <li>
+            <Link to={item.href} >{item.title}</Link>
+        </li>
+    {/if}
+{:else}
+
+{/if}
+
